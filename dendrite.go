551f17147eae3ecc5541535b40b620b158c796e6
Kyle Maxwell <kyle@kylemaxwell.com>
2013-04-13 15:27:12 -0700

    document the yaml

--- dendrite.go
+++ dendrite.go
@@ -5,89 +5,54 @@ import (
 	"bufio"
 	"flag"
 	"github.com/fizx/logs"
-	"github.com/kylelemons/go-gypsy/yaml"
 	"io"
-	"io/ioutil"
+	"log"
 	"os"
-	"path"
-	"strings"
-	"time"
 )
 
 var configFile = flag.String("f", "/etc/dendrite/config.yaml", "location of the config file")
-var debug = false
+var debug = flag.Bool("d", false, "log at DEBUG")
+var logFile = flag.String("l", "/var/log/dendrite.log", "location of the log file")
 
 func main() {
-	flag.BoolVar(&debug, "d", false, "log at DEBUG")
 	flag.Parse()
-	if debug {
+
+	// set the logger path
+	handle, err := os.OpenFile(*logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		logs.Warn("Unable to open log file %s, using stderr: %s", *logFile, err)
+	} else {
+		logs.Logger = log.New(handle, "", log.LstdFlags|log.Lshortfile)
+	}
+
+	// Check whether we're in debug mode
+	if *debug {
 		logs.SetLevel(logs.DEBUG)
 		logs.Debug("logging at DEBUG")
 	} else {
 		logs.SetLevel(logs.INFO)
 	}
-	doc, err := yaml.ReadFile(*configFile)
-	if err != nil {
-		logs.Error("Can't read the config file, error was: ", err)
-	}
 
-	config := new(dendrite.Config)
-	config.Populate(doc)
+	// Read the config file, and link up all of the objects
+	config := dendrite.NewConfig(*configFile)
+	rw := config.CreateReadWriter()
+	groups := config.CreateAllGroups(rw)
 
-	confd := path.Dir(*configFile) + "/conf.d"
-	entries, err := ioutil.ReadDir(confd)
-	if err != nil {
-		logs.Warn("Can't read relevant conf.d: %s", err)
-	} else {
-		for _, entry := range entries {
-			path := confd + "/" + entry.Name()
-			doc, err := yaml.ReadFile(path)
-			if err != nil {
-				logs.Warn("Can't read relevant conf.d: %s", err)
-			} else {
-				base := strings.Replace(entry.Name(), ".yaml", "", 1)
-				config.AddGroup(base, doc.Root)
-			}
-		}
-	}
-
-	out := make(chan string, 0)
-	groups := make([]*dendrite.TailGroup, 0)
-	for _, cg := range config.Groups {
-		groups = append(groups, dendrite.NewTailGroup(cg, out))
-	}
+	// If any of our destinations talk back, log it.
 	go func() {
+		reader := bufio.NewReader(rw)
 		for {
-			for _, g := range groups {
-				g.Poll()
-				time.Sleep(time.Second)
+			str, err := reader.ReadString('\n')
+			if err == io.EOF {
+				logs.Debug("eof")
+			} else if err != nil {
+				logs.Error("error reading: %s", err)
+			} else {
+				logs.Info("received: %s", str)
 			}
 		}
 	}()
 
-	rw, err := dendrite.NewReadWriter(config.Url)
-	if err != nil {
-		panic(err)
-	} else {
-		reader := bufio.NewReader(rw)
-		go func() {
-			for {
-				str, err := reader.ReadString('\n')
-				if err == io.EOF {
-					logs.Debug("eof")
-				} else if err != nil {
-					logs.Error("error reading: %s", err)
-					os.Exit(0)
-				} else {
-					logs.Info("received: %s", str)
-				}
-			}
-		}()
-		for {
-			_, err = rw.Write([]byte(<-out))
-			if err != nil {
-			  logs.Error("error writing: %s", err)
-			}
-		}
-	}
+	// Do the event loop
+	groups.Loop()
 }
--- src/dendrite/config.go
+++ src/dendrite/config.go
@@ -3,9 +3,11 @@ package dendrite
 import (
 	"github.com/fizx/logs"
 	"github.com/kylelemons/go-gypsy/yaml"
+	"io"
 	"net/url"
+	"path"
+	"path/filepath"
 	"regexp"
-	"strconv"
 )
 
 type FieldType int
@@ -29,115 +31,179 @@ type FieldSpec struct {
 	Pattern *regexp.Regexp
 }
 
-type ConfigGroup struct {
+type SourceConfig struct {
 	Glob      string
 	Pattern   string
 	Fields    []FieldSpec
-	Config    Config
 	Name      string
 	OffsetDir string
-	Encoder   Encoder
+}
+
+type DestinationConfig struct {
+	Url *url.URL
 }
 
 type Config struct {
-	Url       *url.URL
-	OffsetDir string
-	Encoder   Encoder
-	Groups    []ConfigGroup
+	OffsetDir    string
+	Destinations []DestinationConfig
+	Sources      []SourceConfig
 }
 
-func (config *Config) AddGroup(name string, group yaml.Node) {
-	logs.Info("Adding group: %s", name)
-	groupMap := group.(yaml.Map)
-	var groupStruct ConfigGroup
-	groupStruct.Name = name
-	groupStruct.Glob = groupMap.Key("glob").(yaml.Scalar).String()
-	groupStruct.Pattern = groupMap.Key("pattern").(yaml.Scalar).String()
-	groupStruct.Fields = make([]FieldSpec, 0)
-	groupStruct.OffsetDir = config.OffsetDir
-	groupStruct.Encoder = config.Encoder
+func (config *Config) CreateReadWriter() io.ReadWriter {
+return nil
+}
 
-	for alias, v := range groupMap.Key("fields").(yaml.Map) {
-		var fieldDetails = v.(yaml.Map)
-		var fieldSpec FieldSpec
-		fieldSpec.Alias = alias
-		fieldSpec.Name = alias
+func (config *Config) CreateAllGroups(rw io.ReadWriter) TailGroups {
+	groups := make([]*TailGroup, 0)
+	// for _, cg := range config.Groups {
+	//     // groups = append(groups, NewTailGroup(cg, out))
+	// }
+	return groups
+}
 
-		tmp, _ := yaml.Child(fieldDetails, ".name")
-		if tmp != nil {
-			fieldSpec.Name = tmp.(yaml.Scalar).String()
-		}
-
-		fieldSpec.Group = -1
-		tmp, _ = yaml.Child(fieldDetails, ".group")
-		if tmp != nil {
-			fieldSpec.Name = ""
-			i, err := strconv.ParseInt(tmp.(yaml.Scalar).String(), 10, 64)
-			if err != nil {
-				logs.Error("error in parsing int", err)
-			}
-
-			fieldSpec.Group = int(i)
-		}
-
-		tmp, _ = yaml.Child(fieldDetails, ".pattern")
-		if tmp != nil {
-			p, err := regexp.Compile(tmp.(yaml.Scalar).String())
-			if err != nil {
-				logs.Error("error in compiling regexp", err)
-			} else {
-				fieldSpec.Pattern = p
-			}
-		}
-
-		tmp, _ = yaml.Child(fieldDetails, ".format")
-		if tmp != nil {
-			fieldSpec.Format = tmp.(yaml.Scalar).String()
-		}
-
-		tmp, _ = yaml.Child(fieldDetails, ".type")
-		if tmp == nil {
-			fieldSpec.Type = String
-		} else {
-			switch tmp.(yaml.Scalar).String() {
-			case "int":
-				fieldSpec.Type = Integer
-			case "gauge":
-				fieldSpec.Type = Gauge
-			case "metric":
-				fieldSpec.Type = Metric
-			case "counter":
-				fieldSpec.Type = Counter
-			case "string":
-				fieldSpec.Type = String
-			case "tokenized":
-				fieldSpec.Type = Tokens
-			case "timestamp", "date":
-				fieldSpec.Type = Timestamp
-			default:
-				logs.Error("Can't recognize field type")
-				panic(nil)
-			}
-		}
-
-		groupStruct.Fields = append(groupStruct.Fields, fieldSpec)
+func NewConfig(configFile string) *Config {
+	doc, err := yaml.ReadFile(configFile)
+	if err != nil {
+		logs.Error("Can't read the config file, error was: ", err)
 	}
-	config.Groups = append(config.Groups, groupStruct)
+
+	config := new(Config)
+	config.Sources = make([]SourceConfig, 0)
+	config.Destinations = make([]DestinationConfig, 0)
+	
+	config.Populate(doc)
+
+	entries, err := filepath.Glob(path.Dir(configFile) + "/conf.d/*.yaml")
+	if err != nil {
+		logs.Warn("Can't read relevant conf.d: %s", err)
+	} else {
+		for _, path := range entries {
+			doc, err := yaml.ReadFile(path)
+			if err != nil {
+				logs.Warn("Can't read relevant conf.d: %s", err)
+			} else {
+				config.Populate(doc)
+			}
+		}
+	}
+	return config
 }
 
 func (config *Config) Populate(doc *yaml.File) {
-	config.Groups = make([]ConfigGroup, 0)
-
-	root := doc.Root.(yaml.Map)
-
-	p, err := doc.Get(".uri")
-	if err != nil {
-		p = "tcp"
-	}
-	config.Url, err = url.Parse(p)
-
-	groups := root.Key("groups")
-	for name, group := range groups.(yaml.Map) {
-		config.AddGroup(name, group)
-	}
+  // root := doc.Root.(yaml.Map)
+  off, err := doc.Get(".global.offset_dir")
+  if err == nil {
+    config.OffsetDir = off
+  }
+  
+  // groups := root.Key("groups")
+  // for name, group := range groups.(yaml.Map) {
+  //  config.AddGroup(name, group)
+  // }
+  
+  
+  // p, err := doc.Get(".uri")
+  // if err != nil {
+  //   p = "tcp"
+  // }
+  // config.Url, err = url.Parse(p)
+  //   tmp, _ = yaml.Child(fieldDetails, ".format")
+  // if tmp != nil {
+  //  fieldSpec.Format = tmp.(yaml.Scalar).String()
+  // }
+  // config.Groups = make([]ConfigGroup, 0)
+  // 
+  // root := doc.Root.(yaml.Map)
+  // 
+  // p, err := doc.Get(".uri")
+  // if err != nil {
+  //  p = "tcp"
+  // }
+  // config.Url, err = url.Parse(p)
+  // 
+  // groups := root.Key("groups")
+  // for name, group := range groups.(yaml.Map) {
+  //  config.AddGroup(name, group)
+  // }
 }
+
+
+func (config *Config) AddGroup(name string, group yaml.Node) {
+  // logs.Info("Adding group: %s", name)
+  // groupMap := group.(yaml.Map)
+  // var groupStruct ConfigGroup
+  // groupStruct.Name = name
+  // groupStruct.Glob = groupMap.Key("glob").(yaml.Scalar).String()
+  // groupStruct.Pattern = groupMap.Key("pattern").(yaml.Scalar).String()
+  // groupStruct.Fields = make([]FieldSpec, 0)
+  // groupStruct.OffsetDir = config.OffsetDir
+  // groupStruct.Encoder = config.Encoder
+  // 
+  // for alias, v := range groupMap.Key("fields").(yaml.Map) {
+  //  var fieldDetails = v.(yaml.Map)
+  //  var fieldSpec FieldSpec
+  //  fieldSpec.Alias = alias
+  //  fieldSpec.Name = alias
+  // 
+  //  tmp, _ := yaml.Child(fieldDetails, ".name")
+  //  if tmp != nil {
+  //    fieldSpec.Name = tmp.(yaml.Scalar).String()
+  //  }
+  // 
+  //  fieldSpec.Group = -1
+  //  tmp, _ = yaml.Child(fieldDetails, ".group")
+  //  if tmp != nil {
+  //    fieldSpec.Name = ""
+  //    i, err := strconv.ParseInt(tmp.(yaml.Scalar).String(), 10, 64)
+  //    if err != nil {
+  //      logs.Error("error in parsing int", err)
+  //    }
+  // 
+  //    fieldSpec.Group = int(i)
+  //  }
+  // 
+  //  tmp, _ = yaml.Child(fieldDetails, ".pattern")
+  //  if tmp != nil {
+  //    p, err := regexp.Compile(tmp.(yaml.Scalar).String())
+  //    if err != nil {
+  //      logs.Error("error in compiling regexp", err)
+  //    } else {
+  //      fieldSpec.Pattern = p
+  //    }
+  //  }
+  // 
+  //  tmp, _ = yaml.Child(fieldDetails, ".format")
+  //  if tmp != nil {
+  //    fieldSpec.Format = tmp.(yaml.Scalar).String()
+  //  }
+  // 
+  //  tmp, _ = yaml.Child(fieldDetails, ".type")
+  //  if tmp == nil {
+  //    fieldSpec.Type = String
+  //  } else {
+  //    switch tmp.(yaml.Scalar).String() {
+  //    case "int":
+  //      fieldSpec.Type = Integer
+  //    case "gauge":
+  //      fieldSpec.Type = Gauge
+  //    case "metric":
+  //      fieldSpec.Type = Metric
+  //    case "counter":
+  //      fieldSpec.Type = Counter
+  //    case "string":
+  //      fieldSpec.Type = String
+  //    case "tokenized":
+  //      fieldSpec.Type = Tokens
+  //    case "timestamp", "date":
+  //      fieldSpec.Type = Timestamp
+  //    default:
+  //      logs.Error("Can't recognize field type")
+  //      panic(nil)
+  //    }
+  //  }
+  // 
+  //  groupStruct.Fields = append(groupStruct.Fields, fieldSpec)
+  // }
+  // config.Groups = append(config.Groups, groupStruct)
+}
+
